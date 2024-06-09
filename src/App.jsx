b7f09f2fcb531bb4cdc5907e2e3a9c33ae8912e1
Hyon Woo Kang <hwkang@logisteq.com>
2024-06-10 01:24:43 +0900

    Player area created in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-
 function App() {
-  
-
   return (
-    <header>
-      <img src="" alt="" />
-      <h1>Tic-Tac-Toe</h1>
-    </header>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
