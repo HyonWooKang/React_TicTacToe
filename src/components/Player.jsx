d8ef940096e16e010080612402ce2590b5194cfe
HyonWooKang <87benzenes@gmail.com>
2024-06-10 13:57:34 +0900

    Player component completed

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,36 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
+    // isEditing을 부정하면 state 업데이트가 예정되어 있기 때문에 다음 코드에서 제대로 동작 안 하는 경우가 생김
+    // 그래서 react에서는 함수형으로 다른 변수를 사용하여 처리하는 것을 추천함
     setIsEditing((editing) => !editing);
-    // state 업데이트가 예정되어 있기 때문에 변수의 부정보다는 함수 형태로 사용하는 것을 추천
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        defaultValue={playerName}
+        onChange={handleChange}
+      />
+    );
   }
 
   return (
     <li>
       <span className="palyer">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
