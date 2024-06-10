e46d3186c720f0897bfaab052d9b0989b68b0770
HyonWooKang <87benzenes@gmail.com>
2024-06-10 11:50:08 +0900

    Separated the Player component

---
+++ src/components/Player.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    // state 업데이트가 예정되어 있기 때문에 변수의 부정보다는 함수 형태로 사용하는 것을 추천
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required value={name} />;
+  }
+
+  return (
+    <li>
+      <span className="palyer">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
